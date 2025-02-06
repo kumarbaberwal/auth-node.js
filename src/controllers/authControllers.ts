bc0342ba549334e347b6b680dca3ac67c8500010
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-02-06 23:58:06 +0530

    authControllers - saltRounds

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -36,7 +36,8 @@ export const register = async (req: Request, res: Response): Promise<any> => {
             });
         }
 
-        const salt = await bcrypt.genSalt(10);
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
